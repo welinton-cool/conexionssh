d139c1fe29582b97c3cb9b35a7dfd971a679f515
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-28 16:07:08 -0500

    comentario ssh

---
+++ src/main/java/ec/edu/ec/mavenproject2/Mavenproject2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.mavenproject2;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        //declarar un vector
+       // int [] enteros;
+        int []enteros={5,7,9}; 
+        
+        //inicializar un vector
+        enteros=new int [3];
+        //inicializar los valores de un vector
+        enteros[0]=5;
+        enteros[1]=7;
+        enteros[2]=9;
+        
+        /*for(int i=0; i<=2;i=i+1){
+            System.out.println("el valor"+enteros[i]+"esta en la posicion[i])}*/
+        int j=enteros[1];
+        System.out.println("el valor de la pocision 1e es :"+j);
+        }
+    
+    }         
+        
+
